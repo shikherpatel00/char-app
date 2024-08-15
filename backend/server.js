eeaa63538cc5dc3a2098f4883ea1b88aba2318c9
Shikher <shikherpatel@gmail.com>
2024-08-15 18:55:22 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./Routes/auth.routes.js";
+import messageRoutes from "./Routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from './db/connectToMongo.js';
+
+const app=express();
+const PORT= process.env.port || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log("Server is running on some port")
+});
